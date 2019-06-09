4b1b44807782194b6feaf25ac408f669d3243760
MikiKru <michal_kruczkowski@o2.pl>
2019-06-09 14:22:44 +0200

    blog v10

--- src/main/java/pl/sda/mysimpleblog/controller/PostsController.java
+++ src/main/java/pl/sda/mysimpleblog/controller/PostsController.java
@@ -9,7 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.sda.mysimpleblog.model.Comment;
 import pl.sda.mysimpleblog.model.Post;
+import pl.sda.mysimpleblog.model.enums.CategoryEnum;
 import pl.sda.mysimpleblog.service.PostsService;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 @Controller
 public class PostsController {
@@ -26,10 +30,6 @@ public class PostsController {
         model.addAttribute("posts",posts);
         return "posts";     // zwracana nazwa widoku html
     }
-    @GetMapping("/addpost")
-    public String addPost(){
-        return "addpost";
-    }
     @GetMapping("/post/{post_id}")
     public String getPost(@PathVariable Long post_id, Model model){
         Post post = postsService.getPostById(post_id);
@@ -44,5 +44,20 @@ public class PostsController {
         postsService.addComment(comment,post_id, user_id);
         // przekierowanie na adres URL nie na nazwę widoku
         return "redirect:/post/" + post_id;
+    }
+    @GetMapping("/addpost")
+    public String addPost(Model model){
+        model.addAttribute("post",new Post());
+        List<CategoryEnum> categories =
+                new ArrayList<>(Arrays.asList(CategoryEnum.values()));
+        System.out.println(categories);
+        // przekazanie listy kategorii do znacznika SELECT
+        model.addAttribute("categories", categories);
+        return "addpost";
+    }
+    @PostMapping("/addpost")
+    public String addPost(@ModelAttribute Post post){
+        // ???
+        return "redirect:/";
     }
 }
