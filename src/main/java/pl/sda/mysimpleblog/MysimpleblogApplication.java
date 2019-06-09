503468f3dbe55a594e599d41f62495b98d192d59
MikiKru <michal_kruczkowski@o2.pl>
2019-06-09 09:41:03 +0200

    blog-init

---
+++ src/main/java/pl/sda/mysimpleblog/MysimpleblogApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.mysimpleblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysimpleblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysimpleblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/mysimpleblog/MysimpleblogApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sda.mysimpleblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MysimpleblogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
