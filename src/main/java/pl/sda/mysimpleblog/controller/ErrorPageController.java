d5c068f087f0826a2f3f6f6a90896cc1c221cddd
MikiKru <michal_kruczkowski@o2.pl>
2019-07-12 14:01:19 +0200

    spring_blog 13.06 v3

--- src/main/java/pl/sda/mysimpleblog/controller/ErrorPageController.java
+++ src/main/java/pl/sda/mysimpleblog/controller/ErrorPageController.java
@@ -1,29 +1,29 @@
-//package pl.sda.mysimpleblog.controller;
-//
-//import org.springframework.boot.web.servlet.error.ErrorController;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class ErrorPageController implements ErrorController {
-//
-//    @Override
-//    public String getErrorPath() {
-//        return "/error";
-//    }
-//
-//    @GetMapping("/error")
-//    public String errorPage(Model model) {
-//        model.addAttribute("info", "Nie ma takiego adresu w naszej domenie.");
-//        return "errorPage";
-//    }
-//
-//    @GetMapping("/errorLogin")
-//    public String errorLogin(Model model) {
-//        model.addAttribute("info", "Błąd logowania!");
-//        return "errorPage";
-//    }
-//
-//}
-//
+package pl.sda.mysimpleblog.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorPageController implements ErrorController {
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @GetMapping("/error")
+    public String errorPage(Model model) {
+        model.addAttribute("info", "Nie ma takiego adresu w naszej domenie.");
+        return "errorPage";
+    }
+
+    @GetMapping("/errorLogin")
+    public String errorLogin(Model model) {
+        model.addAttribute("info", "Błąd logowania!");
+        return "errorPage";
+    }
+
+}
+
