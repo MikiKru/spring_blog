9f6171f9be6ecf27555eccb1289b1d3fc97fd0d6
MikiKru <michal_kruczkowski@o2.pl>
2019-07-13 10:16:00 +0200

    spring_blog 13.06 v4

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,11 @@
 INSERT INTO role values (1, "ROLE_USER");
 INSERT INTO role values (2, "ROLE_ADMIN");
+
+-- /* # email = user and password = user */
+-- INSERT INTO user VALUES (1, 1, 'user', '$2y$12$UxYZe.1YWUJE5wDq82zx8uAUFxYsg2fP5GLPJOYaHDBp0SLl64A/O', '2019-07-13');
+-- /* email = admin and password = admin */
+-- INSERT INTO user VALUES (2, 1, 'admin', '$2y$12$O.KAxmH5iHSKUHlkovvkOOWa218ncUOFmwwleVIyvy57pJMSPNG1q', '2019-07-13');
+--
+-- INSERT INTO user_role VALUES (1, 1);
+-- INSERT INTO user_role VALUES (2, 1);
+-- INSERT INTO user_role VALUES (2, 2);
